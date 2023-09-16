e5c453e28f6f5e2439a134b77b99b6638107f68c
Yl1a <volkova.ylia2004@gmail.com>
2023-09-16 09:13:48 +0300

    t9

--- js/main.js
+++ js/main.js
@@ -16,6 +16,37 @@
     
 })
 
+let duck = document.querySelector('.duck');
+
+document.addEventListener('keydown', function(event){
+    if(event.key.toLowerCase() == 'w'){
+        console.log('up');
+        duck.style.top = duck.offsetTop-50+'px'
+    }else if(event.key.toLowerCase() == 's'){
+        console.log('down');
+        duck.style.top = duck.offsetTop+50+'px'
+    }else if(event.key.toLowerCase() == 'a'){
+        console.log('left');
+        duck.style.left = duck.offsetLeft-50+'px';
+    }else if(event.key.toLowerCase() == 'd'){
+        console.log('right');
+        duck.style.left = duck.offsetLeft+50+'px';
+    }
+
+})
+
+document.addEventListener('keydown', function(event){
+    if(event.key.toLowerCase() == 'r'){
+        console.log('yep');
+        document.body.style.background = "#AE6060";
+    }else if(event.key.toLowerCase() == 'b'){
+        document.body.style.background = "#6B8D9B";
+    }else if(event.key.toLowerCase() == 'g'){
+        document.body.style.background = "#95AE60";
+    }
+})
+
+console.log('hello');
 
 
 /*end*/
