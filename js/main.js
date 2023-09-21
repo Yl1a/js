b5656a3cfd92889ce1b004eb12f28dd03f2c7d7e
Yl1a <volkova.ylia2004@gmail.com>
2023-09-21 13:13:58 +0300

    task10

--- js/main.js
+++ js/main.js
@@ -1,52 +1,67 @@
 /*sm8*/ 
 
-    document.querySelectorAll('.btn').forEach((el) => {
-        el.addEventListener('mousedown', () =>{
+let inputName = document.querySelector('.inputName');
+let inputYear = document.querySelector('.inputYear');
+let btn = document.querySelector('.btn');
+let errName = document.querySelector('.errorName');
+let errYear = document.querySelector('.errorYear');
+let errorReg = document.querySelector('.errorReg');
 
-            let content = el.nextElementSibling;
-            console.log(content);
+let regForm = document.querySelector('.reg');
 
-        if(content.style.maxHeight){
-            document.querySelectorAll('.answer').forEach(el => el.style.maxHeight = null)
-        }else{
-            document.querySelectorAll('.answer').forEach(el => el.style.maxHeight = null)
-            content.style.maxHeight = content.scrollHeight + 'px'
-        }
-    })
+let log = true;
+
+reg = /[A-Za-zA-яА-яЁё]/g;
+
+inputYear.oninput = function(){
+    this.value = this.value.replace(reg, '')
+}
+
+regForm.addEventListener('keyup', function(){
+    if(inputName.value.length < 2 ){
+        inputName.style.border = "2px solid #dc7137";
+        errName.innerHTML = `<p class="error">*Введите не менее 2 символов</p>`;
+        log = false;
+    }else{
+        inputName.style.border = "2px solid #AE9961";
+        errName.innerHTML = ``;
+    }
+
+    if(inputYear.value.length < 4 || inputYear.value.length > 4 ){
+        inputYear.style.border = "2px solid #dc7137";
+        errYear.innerHTML = `<p class="error">*Введите год, 4 цифры</p>`;
+        log = false;
+    }else{
+        inputYear.style.border = "2px solid #AE9961";
+        errYear.innerHTML = ``;
+    }
     
 })
 
-let duck = document.querySelector('.duck');
 
-document.addEventListener('keydown', function(event){
-    if(event.key.toLowerCase() == 'w'){
-        console.log('up');
-        duck.style.top = duck.offsetTop-50+'px'
-    }else if(event.key.toLowerCase() == 's'){
-        console.log('down');
-        duck.style.top = duck.offsetTop+50+'px'
-    }else if(event.key.toLowerCase() == 'a'){
-        console.log('left');
-        duck.style.left = duck.offsetLeft-50+'px';
-    }else if(event.key.toLowerCase() == 'd'){
-        console.log('right');
-        duck.style.left = duck.offsetLeft+50+'px';
+
+
+btn.onclick = function(event){
+    if (inputName.value.length === 0){
+        errName.innerHTML =`<p class="error">*Введите имя</p>`;
+        log = false;
     }
+    if (inputYear.value.length === 0){
+        errYear.insertAdjacentHTML('beforeend', 
+        `<p class="error">*Введите год рождения</p>`)
+        log = false;
 
-})
-
-document.addEventListener('keydown', function(event){
-    if(event.key.toLowerCase() == 'r'){
-        console.log('yep');
-        document.body.style.background = "#AE6060";
-    }else if(event.key.toLowerCase() == 'b'){
-        document.body.style.background = "#6B8D9B";
-    }else if(event.key.toLowerCase() == 'g'){
-        document.body.style.background = "#95AE60";
     }
-})
+/*     if(log == true){
+        inputName.value = '';
+        inputYear.value = '';
+    }else{
+        errorReg.innerHTML = 'регистрация не прошла';
+    } */
+    event.preventDefault();
+    
 
-console.log('hello');
+}
 
 
 /*end*/
