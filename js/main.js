4731f482ae1370940153e9eb934da282bca3bb97
Yl1a <volkova.ylia2004@gmail.com>
2023-09-22 08:45:55 +0300

    t10

--- js/main.js
+++ js/main.js
@@ -9,9 +9,10 @@ let errorReg = document.querySelector('.errorReg');
 
 let regForm = document.querySelector('.reg');
 
-let log = true;
 
 reg = /[A-Za-zA-яА-яЁё]/g;
+
+log = true;
 
 inputYear.oninput = function(){
     this.value = this.value.replace(reg, '')
@@ -22,9 +23,11 @@ regForm.addEventListener('keyup', function(){
         inputName.style.border = "2px solid #dc7137";
         errName.innerHTML = `<p class="error">*Введите не менее 2 символов</p>`;
         log = false;
+
     }else{
         inputName.style.border = "2px solid #AE9961";
         errName.innerHTML = ``;
+        log = true;
     }
 
     if(inputYear.value.length < 4 || inputYear.value.length > 4 ){
@@ -34,7 +37,11 @@ regForm.addEventListener('keyup', function(){
     }else{
         inputYear.style.border = "2px solid #AE9961";
         errYear.innerHTML = ``;
+        log = true;
     }
+
+/*  */
+    
     
 })
 
@@ -43,23 +50,21 @@ regForm.addEventListener('keyup', function(){
 
 btn.onclick = function(event){
     if (inputName.value.length === 0){
+        log = false;
         errName.innerHTML =`<p class="error">*Введите имя</p>`;
-        log = false;
-    }
-    if (inputYear.value.length === 0){
-        errYear.insertAdjacentHTML('beforeend', 
-        `<p class="error">*Введите год рождения</p>`)
-        log = false;
 
     }
-/*     if(log == true){
+    if (inputYear.value.length === 0){
+        log = false;
+        errYear.innerHTML =`<p class="error">*Введите год</p>`;
+    }
+
+    event.preventDefault();
+
+    if(log == true){
         inputName.value = '';
         inputYear.value = '';
-    }else{
-        errorReg.innerHTML = 'регистрация не прошла';
-    } */
-    event.preventDefault();
-    
+    }
 
 }
 
